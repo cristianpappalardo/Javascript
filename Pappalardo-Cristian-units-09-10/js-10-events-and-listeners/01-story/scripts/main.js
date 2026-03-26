a3c15f5e5b6c140bef424a0421e92dde382b08ea
cristianpappalardo <cristian.pappalardo@edu-its.it>
2026-03-26 12:40:01 +0100

    Added jsdoc comments to main.js files in 01-story

--- Pappalardo-Cristian-units-09-10/js-10-events-and-listeners/01-story/scripts/main.js
+++ Pappalardo-Cristian-units-09-10/js-10-events-and-listeners/01-story/scripts/main.js
@@ -1,3 +1,12 @@
+/**
+ * @file main.js
+ * @author Cristian Pappalardo
+ * 01 Story exercise
+ * 
+ * @description This JavaScript file contains a function to generate a story based on user input for a noun, an adjective, and a person's name. 
+ * It also includes an event listener for a button that triggers the story generation and displays the result in a designated area on the webpage.
+ */
+
 const button = document.getElementById('gen-button');
 if (button) {
     button.addEventListener('click', makeStory);
@@ -7,6 +16,11 @@ if (button) {
     const personInput = document.getElementById('person');
     const storyElement = document.getElementById('story');
 
+    /**
+     * Checks if all required elements are present in the DOM.
+     * @returns {boolean} true if all elements are present, undefined otherwise
+     */
+
     function checkElements() {
         if (!nounInput || !adjectiveInput || !personInput || !storyElement) {
             console.error('One or more elements not found. Please check the HTML structure.');
@@ -15,6 +29,11 @@ if (button) {
         return true;
         }
 
+    /**
+     * Generates a story based on user input.
+     * @returns undefined if checkElements fails, otherwise displays the generated story in the designated area on the webpage.
+     */
+    
     function makeStory() {
         if (!checkElements()) {
             return;
